51923e3afcb247875e23051a1712370ddd9a721f
shenkev <tingkekevin@hotmail.com>
2017-11-05 13:08:46 -0500

    Logging for hidden units in model

--- birds/data_util.py
+++ birds/data_util.py
@@ -136,7 +136,8 @@ def load_simple_yes_no_qa_pairs_helper(qa_path, attributes):
     for qa_pair in qa_pairs:
         qa_pair[1] = attributes[qa_pair[1]]
 
-    max_questions = 5000
+    # TODO fix this up, this is hacky
+    max_questions = 50000
 
     # Python 3 Version
     # qa_pairs.sort(key= lambda qa_pair: qa_pair[2])
@@ -152,7 +153,6 @@ def load_simple_yes_no_qa_pairs_helper(qa_path, attributes):
         else:
             return 1
 
-    # TODO fix this up, this is hacky
     qa_pairs.sort(cmp=answer_compare)
     end_of_ones_idx = (i for i, v in enumerate(qa_pairs) if v[2]==0).next()
     qa_pairs = qa_pairs[0:min(end_of_ones_idx, max_questions/2)] \
--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from torch.nn import Parameter
 from torch.autograd import Variable
 
 
-def embed_question(question, embed_layer, position_encoding):
+def embed_question(question, embed_layer, position_encoding=None):
     question_emb = embed_layer(question)
 
     # question_emb = question_emb * position_encoding
@@ -13,18 +13,18 @@ def embed_question(question, embed_layer, position_encoding):
     return question_emb.sum(1)
 
 
-def embed_evidence(evidence, question_embed_layer, evidence_embed_layer, position_encoding):
+def embed_evidence(evidence, question_embed_layer, evidence_embed_layer, position_encoding=None):
     expanded_evidence = evidence.view(-1, evidence.size(2))  # combine batch and sentence dimensions
 
     evidence_for_computation = question_embed_layer(expanded_evidence)
     # evidence_for_computation = evidence_for_computation * position_encoding
     evidence_for_computation = evidence_for_computation.sum(1)
-    evidence_for_computation = evidence_for_computation.view(-1, evidence.size(1), position_encoding.size(2))
+    evidence_for_computation = evidence_for_computation.view(-1, evidence.size(1), evidence_embed_layer.embedding_dim)
 
     evidence_features = evidence_embed_layer(expanded_evidence)
     # evidence_features = evidence_features * position_encoding
     evidence_features = evidence_features.sum(1)
-    evidence_features = evidence_features.view(-1, evidence.size(1), position_encoding.size(2))
+    evidence_features = evidence_features.view(-1, evidence.size(1), evidence_embed_layer.embedding_dim)
 
     return evidence_features, evidence_for_computation
 
@@ -89,7 +89,7 @@ class vqa_memnet(nn.Module):
     def __init__(self, vocabulary_size, text_latent_size, words_in_question):
         super(vqa_memnet, self).__init__()
 
-        self.position_encoding = get_position_encoding(words_in_question, text_latent_size)
+        # self.position_encoding = get_position_encoding(words_in_question, text_latent_size)
 
         # self.temporal_enc1 = Parameter(torch.Tensor(num_of_evidences, text_latent_size))
         # self.temporal_enc2 = Parameter(torch.Tensor(num_of_evidences, text_latent_size))
@@ -103,17 +103,16 @@ class vqa_memnet(nn.Module):
         self.evidence_emb.weight.data.normal_(0, 0.1)
         self.question_emb.weight.data.normal_(0, 0.1)
 
-        self.fc1 = nn.Linear(2*text_latent_size, 2)
+        self.fc1 = nn.Linear(text_latent_size, 2)
         # self.prelu = nn.PReLU()
         # self.fc2 = nn.Linear(20, 2)
 
         self.softmax = nn.Softmax()
 
-    def forward(self, evidence, question):
+    def forward(self, evidence, question, logger=None, iter=None):
 
-        question_emb = embed_question(question, self.question_emb, self.position_encoding)
-        evidence_feature_emb, evidence_computation_emb =\
-            embed_evidence(evidence, self.question_emb, self.evidence_emb, self.position_encoding)
+        question_emb = embed_question(question, self.question_emb)
+        evidence_feature_emb, evidence_computation_emb = embed_evidence(evidence, self.question_emb, self.evidence_emb)
 
         # evidence_feature_emb = evidence_feature_emb + self.temporal_enc1
         # evidence_computation_emb = evidence_computation_emb + self.temporal_enc2
@@ -121,11 +120,21 @@ class vqa_memnet(nn.Module):
         weights = compute_evidence_weights(evidence_computation_emb, question_emb)
         weighted_evidence = mean_pool(evidence_feature_emb, weights)
 
+        # features = torch.cat((weighted_evidence, question_emb.squeeze(0)), 1)
+        features = weighted_evidence + question_emb.squeeze(0)
 
-        features = torch.cat((weighted_evidence, question_emb.squeeze(0)), 1)
-        # features = weighted_evidence + question_emb.squeeze(0)
+        if logger is not None:
+            logger.histo_summary('H_question_emb', to_np(question_emb), iter)
+            logger.histo_summary('H_evidence_computation_emb', to_np(evidence_computation_emb), iter)
+            logger.histo_summary('H_evidence_feature_emb', to_np(evidence_feature_emb), iter)
+            logger.histo_summary('H_max_weights', to_np(torch.max(weights, 1)[0]), iter)
+            logger.histo_summary('H_features', to_np(features), iter)
 
         output = self.fc1(features)
         # output = self.prelu(output)
         # output = self.fc2(output)
         return output
+
+
+def to_np(x):
+    return x.data.cpu().numpy()
